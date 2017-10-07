55b2f008de269b794d982ee274d4bb2c16e2869c
giva_ <giva_@DESKTOP-0MJO86D>
2017-10-07 11:58:58 -0300

    Edição da Classe Dono (Treino)

--- src/classes_Iniciais/Dono.java
+++ src/classes_Iniciais/Dono.java
@@ -2,21 +2,21 @@ package classes_Iniciais;
 
 public class Dono {
 	
-	private String nome;
+	private String NomeDoMsr;
 
-	public String getNome() {
-		return nome;
+	public String getNomeDoMsr() {
+		return NomeDoMsr;
 	}
 
-	public void setNome(String nome) {
-		this.nome = nome;
+	public void setNomeDoMsr(String nomeDoMsr) {
+		NomeDoMsr = nomeDoMsr;
 	}
 
 	@Override
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
-		result = prime * result + ((nome == null) ? 0 : nome.hashCode());
+		result = prime * result + ((NomeDoMsr == null) ? 0 : NomeDoMsr.hashCode());
 		return result;
 	}
 
@@ -29,15 +29,19 @@ public class Dono {
 		if (getClass() != obj.getClass())
 			return false;
 		Dono other = (Dono) obj;
-		if (nome == null) {
-			if (other.nome != null)
+		if (NomeDoMsr == null) {
+			if (other.NomeDoMsr != null)
 				return false;
-		} else if (!nome.equals(other.nome))
+		} else if (!NomeDoMsr.equals(other.NomeDoMsr))
 			return false;
 		return true;
 	}
 	
+ // System.out.println("caralhooo");
+
+}
+	
+	
 	
 	
 
-}
